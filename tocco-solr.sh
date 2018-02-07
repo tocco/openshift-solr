9382f7c8dec3d6170519daabbbd5b32699bd2b1a
Peter Gerber <peter@arbitrary.ch>
2018-02-07 17:47:08 +0100

    automatically determine heap memory size
    When a memory request is set, use it to calculate the how much
    heap memory should be assigned to Java.

--- tocco-solr.sh
+++ tocco-solr.sh
@@ -10,5 +10,11 @@ rm -f /persist/index_data/index/write.lock
 # (GC_TUNE, unlike its name suggests, can be used for any Java option)
 echo 'GC_TUNE="-XX:+ExitOnOutOfMemoryError${GC_TUNE+ $GC_TUNE}"' >>/opt/solr/bin/solr.in.sh
 
+# set heap memory based on requested memory
+if [ -z "${SOLR_PARAM_SOLR_HEAP-}" ] && [ -n "${REQUESTED_MEMORY-}" ]; then
+    memory_factor=${MEMORY_FACTOR-0.52}
+    export SOLR_PARAM_SOLR_HEAP=$(awk 'BEGIN { printf "%dm", '$REQUESTED_MEMORY'/1048576 * '$memory_factor' }')
+fi
+
 # entrypoint script shipped with Solr
 exec docker-entrypoint.sh "$@"
