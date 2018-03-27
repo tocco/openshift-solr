33a33c07123577f38cde0cb53d44013d578d1c79
Peter Gerber <peter@arbitrary.ch>
2018-03-27 11:25:48 +0200

    Set default log level to warn

--- tocco-solr.sh
+++ tocco-solr.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -Eeu
 
+# set custom default log level
+export SOLR_PARAM_SOLR_LOG_LEVEL=WARN
+
 rm -f /persist/index_data/index/write.lock
 
 # write config file based on environment variables
